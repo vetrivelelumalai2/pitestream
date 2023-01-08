6b06b67bcca3f5a45e468f1820064e9d5947e171
vetrivelelumalai2 <68172178+vetrivelelumalai2@users.noreply.github.com>
2023-01-08 10:30:39 +0100

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -24,7 +24,7 @@ async def root_route_handler(_):
         {
             "server_status": "running",
             "uptime": utils.get_readable_time(time.time() - StartTime),
-            "telegram_bot": "@" + StreamBot.username,
+            
             "connected_bots": len(multi_clients),
             "loads": dict(
                 ("bot" + str(c + 1), l)
