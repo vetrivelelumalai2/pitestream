55c5d3fe674a6dc1e9345cf3b5663e68ce92817e
vetrivelelumalai2 <68172178+vetrivelelumalai2@users.noreply.github.com>
2023-01-08 10:43:10 +0100

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -28,8 +28,8 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 async def media_receive_handler(_, m: Message):
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     file_hash = get_hash(log_msg, Var.HASH_LENGTH)
-    stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
-    short_link = f"{Var.URL}{file_hash}{log_msg.id}"
+    stream_link = f"https://killadiclub.xyz/api?api=42907ef73e4b9e28698e3b042c4ad6a5861681a5&url={Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
+    short_link = f"https://killadiclub.xyz/api?api=42907ef73e4b9e28698e3b042c4ad6a5861681a5&url={Var.URL}{file_hash}{log_msg.id}"
     logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     try:
         await m.reply_text(
